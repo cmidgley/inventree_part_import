cmidgley/inventree_part_import

commit 68f40ac546664850ee13d15bdabb80e76fc438c7

Format code, Rename ipn_template to ipn_format, Refactor IPNSetting

inventree_part_import/part_importer.py
@@ -6,10 +6,10 @@ from string import Formatter, _string
 from cutie import select
 from inventree.company import Company, ManufacturerPart, SupplierPart, SupplierPriceBreak
 from inventree.part import Parameter, Part
+from jinja2 import Template
 from requests.compat import quote
 from requests.exceptions import HTTPError
 from thefuzz import fuzz
-from jinja2 import Template
 
 from .categories import setup_categories_and_parameters
 from .config import CATEGORIES_CONFIG, CONFIG, get_config, get_pre_creation_hooks
@@ -29,13 +29,10 @@ class ImportResult(Enum):
     def __or__(self, other):
         return self if self.value < other.value else other
 
-class IPNSetting(Enum):
-    NEVER = 0
-    NEW = 1
-    ALWAYS = 2
+IPNSetting = Enum("false", "true", "overwrite")
 
 class PartImporter:
-    def __init__(self, inventree_api, interactive=False, verbose=False, ipn=IPNSetting.NEW):
+    def __init__(self, inventree_api, interactive=False, verbose=False, ipn=IPNSetting.true):
         self.api = inventree_api
         self.interactive = interactive
         self.verbose = verbose
@@ -155,9 +152,9 @@ class PartImporter:
 
     def import_part_ipn(self, api_part, supplier):
         # only add IPN if --ipn ALWAYS, or --ipn NEW and part doesn't yet have an IPN
-        if self.ipn == IPNSetting.NEVER:
+        if self.ipn == IPNSetting.false:
             return True
-        if self.ipn == IPNSetting.NEW and hasattr(self.existing_part, "IPN") and self.existing_part.IPN:
+        if self.ipn == IPNSetting.true and getattr(self.existing_part, "IPN", None):
             return True
 
         # find the outer most mapped category (the leaf category)
@@ -170,12 +167,11 @@ class PartImporter:
         else:
             return True
 
-        # locate the closest template in category heirarchy
-        ipn_template = None
+        # locate the closest template in category hierarchy
+        ipn_format = None
         for subcategory in reversed(category.path):
-            mapped_subcategory = self.category_map.get(subcategory.lower())
-            if mapped_subcategory: # and hasattr(mapped_subcategory, "ipn_template") and mapped_subcategory.ipn_template and mapped_subcategory.ipn_template.strip():
-                ipn_template = mapped_subcategory.ipn_template
+            if mapped_subcategory := self.category_map.get(subcategory.lower()):
+                ipn_format = mapped_subcategory.ipn_format
                 break
         else:
             return True
@@ -186,15 +182,19 @@ class PartImporter:
             "supplier": supplier.name,
             "category": category.name if category else "",
             "parameters": api_part.parameters,
-            **{attr: getattr(api_part, attr) for attr in dir(api_part) if not attr.startswith('_') and not callable(getattr(api_part, attr))},
+            **{
+                attr: getattr(api_part, attr)
+                for attr in dir(api_part)
+                if not attr.startswith('_') and not callable(getattr(api_part, attr))
+            },
         }
 
         # render the IPN template
         try:
-            template = Template(ipn_template)
+            template = Template(ipn_format)
             ipn = template.render(context)
         except Exception as e:
-            error(f"failed to render IPN template '{ipn_template}' with: {e}")
+            error(f"failed to render IPN template '{ipn_format}' with: {e}")
             return False
 
         # if we got a resulting ipn, other than just separators, update the part
@@ -209,7 +209,7 @@ class PartImporter:
                 update_object_data(self.existing_part, {"IPN": ipn})
 
             if self.verbose or self.dry_run:
-                info(f"set part IPN to '{ipn}' using template '{ipn_template}'")
+                info(f"set part IPN to '{ipn}' using template '{ipn_format}'")
 
         return True
 

inventree_part_import/categories.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass, field
 
 from inventree.part import ParameterTemplate, PartCategory, PartCategoryParameterTemplate
 
-from .config import (CATEGORIES_CONFIG, PARAMETERS_CONFIG, get_categories_config,
-                     get_parameters_config, update_config_file, get_config)
+from .config import (CATEGORIES_CONFIG, PARAMETERS_CONFIG, get_categories_config, get_config,
+                     get_parameters_config, update_config_file)
 from .error_helper import *
 
 def setup_categories_and_parameters(inventree_api):
@@ -161,7 +161,7 @@ class Category:
     ignore: bool
     structural: bool
     aliases: list[str] = field(default_factory=list)
-    ipn_template: str = ""
+    ipn_format: str = ""
     parameters: list[str] = field(default_factory=list)
     part_category: PartCategory = None
 
@@ -195,7 +195,9 @@ class Category:
                     f"'{CATEGORIES_CONFIG}'"
                 )
 
-CATEGORY_ATTRIBUTES = {"_parameters", "_description", "_ignore", "_structural", "_aliases", "_ipn_template"}
+CATEGORY_ATTRIBUTES = {
+    "_parameters", "_description", "_ignore", "_structural", "_aliases", "_ipn_format",
+}
 def parse_category_recursive(categories_dict, parameters=tuple(), path=tuple(), parent=None):
     if not categories_dict:
         return {}
@@ -215,21 +217,23 @@ def parse_category_recursive(categories_dict, parameters=tuple(), path=tuple(), 
             if child.startswith("_") and child not in CATEGORY_ATTRIBUTES:
                 warning(f"ignoring unknown special attribute '{child}' in category '{name}'")
 
+        default_ipn_format = parent.ipn_format if parent else get_config().get("ipn_format")
+
         new_parameters = parameters + tuple(values.get("_parameters", []))
         new_path = path + (name,)
 
-        categories[new_path] = Category(
+        categories[new_path] = category = Category(
             name=name,
             path=list(new_path),
             description=values.get("_description", name),
             ignore=values.get("_ignore", False),
             structural=values.get("_structural", False),
             aliases=values.get("_aliases", []),
-            ipn_template=values.get("_ipn_template", get_config().get("ipn_template", "") if parent is None else parent.ipn_template),
+            ipn_format=values.get("_ipn_format", default_ipn_format),
             parameters=new_parameters,
         )
 
-        categories.update(parse_category_recursive(values, new_parameters, new_path, categories[new_path]))
+        categories.update(parse_category_recursive(values, new_parameters, new_path, category))
 
     return categories
 

inventree_part_import/cli.py
@@ -14,7 +14,7 @@ from .config import (CONFIG, SUPPLIERS_CONFIG, get_config, get_config_dir, set_c
                      setup_inventree_api, update_config_file, update_supplier_config)
 from .error_helper import *
 from .inventree_helpers import get_category, get_category_parts
-from .part_importer import ImportResult, PartImporter, IPNSetting
+from .part_importer import ImportResult, IPNSetting, PartImporter
 from .suppliers import get_suppliers, setup_supplier_companies
 
 def handle_errors(func):
@@ -44,7 +44,7 @@ SuppliersChoices = click.Choice(_suppliers.keys(), case_sensitive=False)
 AvailableSuppliersChoices = click.Choice(_available_suppliers.keys(), case_sensitive=False)
 
 InteractiveChoices = click.Choice(("default", "false", "true", "twice"), case_sensitive=False)
-IPNChoices = click.Choice(("new", "never", "always"), case_sensitive=False)
+IPNChoices = click.Choice(("false", "true", "overwrite"), case_sensitive=False)
 
 @click.command
 @click.pass_context
@@ -55,12 +55,12 @@ IPNChoices = click.Choice(("new", "never", "always"), case_sensitive=False)
     "Enable interactive mode. 'twice' will run once normally, then rerun in interactive "
     "mode for any parts that failed to import correctly."
 ))
+@click.option("-I", "--ipn", type=IPNChoices, default="true",
+    help="Set internal part number according to ipn_format."
+)
 @click.option("-d", "--dry", is_flag=True, help="Run without modifying InvenTree database.")
 @click.option("-c", "--config-dir", help="Override path to config directory.")
 @click.option("-v", "--verbose", is_flag=True, help="Enable verbose output for debugging.")
-@click.option("--ipn", type=IPNChoices, default="new", help="Update IPN mode.  'new' (default) will add IPN only when "
-    "part has none, 'never' will never update IPN and 'always' will update it for all parts.  Requires IPN templates "
-    "to have been configured.")
 @click.option("--show-config-dir", is_flag=True, help="Show path to config directory and exit.")
 @click.option("--configure", type=AvailableSuppliersChoices, help="Configure supplier.")
 @click.option("--update", metavar="CATEGORY", help="Update all parts from InvenTree CATEGORY.")
@@ -196,7 +196,9 @@ def inventree_part_import(
     # make sure suppliers.yaml exists
     get_suppliers(reload=True)
     setup_supplier_companies(inventree_api)
-    importer = PartImporter(inventree_api, interactive=interactive == "true", verbose=verbose, ipn = IPNSetting[ipn.upper()])
+    importer = PartImporter(
+        inventree_api, interactive=interactive == "true", verbose=verbose, ipn=IPNSetting[ipn]
+    )
 
     if update or update_recursive:
         info(f"updating {len(parts)} parts from '{category_path}'", end="\n")

inventree_part_import/config/__init__.py
@@ -115,7 +115,7 @@ VALID_CONFIG_VARS = {
     "datasheets",
     "interactive_category_matches",
     "interactive_parameter_matches",
-    "ipn_template",
+    "ipn_format",
     "part_selection_format",
     "auto_detect_columns",
     *DEFAULT_CONFIG_VARS,

commit 90640f7a8abf54a9a01020fdef2eb5244eca7287

Fix type error on IPNSetting enum (introduced in commit 68f40ac)

inventree_part_import/part_importer.py
@@ -29,7 +29,7 @@ class ImportResult(Enum):
     def __or__(self, other):
         return self if self.value < other.value else other
 
-IPNSetting = Enum("false", "true", "overwrite")
+IPNSetting = Enum('IPNSetting', ["false", "true", "overwrite"])
 
 class PartImporter:
     def __init__(self, inventree_api, interactive=False, verbose=False, ipn=IPNSetting.true):